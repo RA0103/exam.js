f40e5988ff5250c68cd6915da470e1d653512852
ilyosbek <ilyosbek.inomjonov.1996@gmail.com>
2021-12-23 00:22:15 +0500

    done

---
+++ index.js
@@ -0,0 +1,10 @@
+// O'quvchini exam topshirishi
+
+function app(mark) {
+    if (mark === 'A') console.log('passed');
+    else if (mark === 'B') console.log('passed hardly');
+    else if (mark === 'C') console.log('reexam');
+    else if (mark === 'E') console.log('failed');
+    else console.log('enter valid input');
+}
+app('E`')
